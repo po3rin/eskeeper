cf8a4036b2920c1414d36a03a82457476b113011
中村 弘武 <abctail30@gmail.com>
2021-08-31 19:56:07 +0900

    retry config

--- es.go
+++ es.go
@@ -2,7 +2,9 @@ package eskeeper
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
@@ -12,10 +14,21 @@ type esclient struct {
 }
 
 func newEsClient(urls []string, user, pass string) (*esclient, error) {
+	retryBackoff := backoff.NewExponentialBackOff()
+	retryBackoff.InitialInterval = time.Second
+
 	conf := elasticsearch.Config{
-		Addresses: urls,
-		Username:  user,
-		Password:  pass,
+		Addresses:     urls,
+		Username:      user,
+		Password:      pass,
+		RetryOnStatus: []int{408, 429, 502, 503, 504},
+		RetryBackoff: func(i int) time.Duration {
+			if i == 1 {
+				retryBackoff.Reset()
+			}
+			d := retryBackoff.NextBackOff()
+			return d
+		},
 	}
 	es, err := elasticsearch.NewClient(conf)
 	if err != nil {
